50da6fd699aa457667b44d861e2b9b675aab7bfb
linagapa <57391450+linagapa@users.noreply.github.com>
2021-09-28 16:09:37 +0200

    global variables added

---
+++ R/globals.R
@@ -0,0 +1,6 @@
+utils::globalVariables(c("points", "matplot", "domain.to.genome",
+                         "V4", "V5", "y1", "y2", "chr_start",
+                         "chr_end", "Domain", "x1", "x2",
+                         "modelANOVA", "modelLINEAR", "modelLINEAR_CROSS",
+                         "write.table", "SlicedData", "x", "J1pass",
+                         "min.expressed", "normalize.method"))
