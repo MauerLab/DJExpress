28ae71d2211a860f550ce41f5a4319167affc0d4
linagapa <57391450+linagapa@users.noreply.github.com>
2021-09-28 16:01:49 +0200

    DJEanlz added

---
+++ R/DJEanlz.R
@@ -0,0 +1,19 @@
+#' output object of DJEanalize function
+#'
+#' A list of objects containing the results of differential junction expression analysis. TCGA and GTEx example junction expression is used.
+#'
+#' @docType data
+#'
+#' @usage data(DJEanlz)
+#'
+#' @format An object of class \code{"list"}.
+#'
+#' @keywords datasets
+#'
+#' @references Kahles et al.Cancer Cell (2018) Aug 13;34(2):211-224.e6.
+#' (\href{https://pubmed.ncbi.nlm.nih.gov/30078747/}{PubMed})
+#'
+#' @examples
+#' data(DJEanlz)
+#' iPlot.out <- DJEplotSplice(DJEanlz, geneID="RPSA")
+"DJEanlz"
